50549b88ad1676d3c405eeeb0524ad2cd4f17f36
Gabriel-Bosio <gabriel.bosio@hotmail.com>
2025-04-11 20:03:45 -0300

    Ajuste impressão

--- GrafosT2M1/Program.cs
+++ GrafosT2M1/Program.cs
@@ -7,28 +7,31 @@ internal class Program
         GrafoMatriz grafoM = null;
         GrafoLista grafoL = null;
 
-        LeitorGrafo leitor = new LeitorGrafo(".\\..\\..\\..\\grafo.txt");
+        //LeitorGrafo leitor = new LeitorGrafo(".\\..\\..\\..\\grafo.txt");
+        LeitorGrafo leitor = new LeitorGrafo(".\\..\\..\\..\\grafoAva.txt");
 
         leitor.GeraGrafo(ref grafoM);
         
         grafoM.ImprimeGrafo();
 
-        grafoM.ImprimeBusca( grafoM.RetornarBuscaProfundidade(5), false);
+        grafoM.ImprimeBusca( grafoM.RetornarBuscaProfundidade(0), false);
 
-        grafoM.ImprimeBusca(grafoM.RetornarBuscaLargura(5), true);
+        grafoM.ImprimeBusca(grafoM.RetornarBuscaLargura(0), true);
 
-        grafoM.ImprimeBusca(grafoM.RetornarDijkstra(5));
+        grafoM.ImprimeBusca(grafoM.RetornarDijkstra(0));
 
+
+        Console.WriteLine("\n\n\n\n");
 
         leitor.GeraGrafo(ref grafoL);
 
         grafoL.ImprimeGrafo();
 
-        grafoL.ImprimeBusca(grafoL.RetornarBuscaProfundidade(5), false);
+        grafoL.ImprimeBusca(grafoL.RetornarBuscaProfundidade(0), false);
 
-        grafoL.ImprimeBusca(grafoL.RetornarBuscaLargura(5), true);
+        grafoL.ImprimeBusca(grafoL.RetornarBuscaLargura(0), true);
 
-        grafoL.ImprimeBusca(grafoL.RetornarDijkstra(5));
+        grafoL.ImprimeBusca(grafoL.RetornarDijkstra(0));
 
         Console.WriteLine("\n\n\n\n");
     }
